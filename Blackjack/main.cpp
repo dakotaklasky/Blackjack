b805fe7049be73dfcae1c128355856dce7ef3588
dakotaklasky <lolgirllove2shop@gmail.com>
2017-03-07 00:13:30 -0800

    Initial Commit

---
+++ Blackjack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blackjack
+//
+//  Created by Dakota Klasky on 3/7/17.
+//  Copyright © 2017 Dakota Klasky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
